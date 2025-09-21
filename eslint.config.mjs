cc9e2cc5aa4fd379f2e00dc99a498ac377141bfe
AlexandreMaycon <alexsantos13@gmail.com>
2025-09-21 18:48:12 -0300

    change lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,18 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser }
+  },
+  {
+    // reconhece os globals do Jest nos testes
+    files: ["tests/**/*.js", "**/*.test.js"],
+    env: { jest: true },
+    languageOptions: {
+      globals: { ...globals.jest }
+    }
+  }
 ]);
